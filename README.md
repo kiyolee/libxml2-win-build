0776d8880985e42daeea3ed21a621167d7b12bed
Kelvin Lee <kiyolee@gmail.com>
2024-05-16 23:07:22 +1000

    Update to libxml-2.12.7

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 libxml2 Windows build with Visual Studio.
 
-This version is libxml2-2.12.6.
+This version is libxml2-2.12.7.
 
 Note that LZMA support is only available for VS2013 or later.
 
--- xmllint.c
+++ xmllint.c
@@ -563,7 +563,7 @@ xmlHTMLPrintFileContext(xmlParserInputPtr input) {
     len = strlen(buffer);
     snprintf(&buffer[len], sizeof(buffer) - len, "\n");
     cur = input->cur;
-    while ((*cur == '\n') || (*cur == '\r'))
+    while ((cur > base) && ((*cur == '\n') || (*cur == '\r')))
 	cur--;
     n = 0;
     while ((cur != base) && (n++ < 80)) {
@@ -3353,7 +3353,7 @@ main(int argc, char **argv) {
 	else if ((!strcmp(argv[i], "-pedantic")) ||
 	         (!strcmp(argv[i], "--pedantic"))) {
 	    options |= XML_PARSE_PEDANTIC;
-            options &= XML_PARSE_NOWARNING;
+            options &= ~XML_PARSE_NOWARNING;
         }
 #ifdef LIBXML_DEBUG_ENABLED
 	else if ((!strcmp(argv[i], "-debugent")) ||
